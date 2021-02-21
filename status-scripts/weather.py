d52610aa25192285cd7718117c06aeae242cd854
rojan <rojansinha@gmail.com>
2021-02-21 21:37:53 +0545

    weather script added

---
+++ status-scripts/weather.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import requests
+import json
+
+temperature = None
+description = None
+try:
+    response = requests.get('https://openweathermap.org/data/2.5/weather?id=1283240&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02')
+    content = response.text
+    data = json.loads(content)
+    temperature = data['main']['temp']
+    description = data['weather'][0]['description']
+except Exception as e:
+    pass
+
+if temperature is not None:
+    print(" KTM: {}°C - {} ".format(temperature, description))
+
